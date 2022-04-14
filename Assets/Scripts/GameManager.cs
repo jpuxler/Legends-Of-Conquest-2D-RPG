ca17f4ad2b52df55829c80c5ff1d6d9f38054908
Jonathan Puxler <software@puxler.de>
2022-04-15 01:28:15 +0200

    menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    [SerializeField] private PlayerStats[] playerStats;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+
+        playerStats = FindObjectsOfType<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
